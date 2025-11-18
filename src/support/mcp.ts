9955349999804cf730707a656ae8664940067f9b
Simon <s.cabotse@gmail.com>
2025-11-18 14:27:55 +0100

    Update mcp.ts

--- src/support/mcp.ts
+++ src/support/mcp.ts
@@ -3,7 +3,7 @@ import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 
 export const serverJsonInfos = {
     name: 'P-link',
-    version: '0.9.1',
+    version: '0.9.2',
 }
 export function createPLinkMCPserver() {
     return new McpServer(serverJsonInfos);
